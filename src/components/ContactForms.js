20e0f7573ab6626816240dd1494dfa02e67acf44
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-04-26 09:06:09 +0100

    Environment Setup

---
+++ src/components/ContactForms.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactForm = (props) => {
+  return (
+    <div>from Contacts form</div>
+  )
+}
+
+export default ContactForm;
---
+++ src/components/Contacts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contacts = (props) => {
+  return (
+    <div>form Contacts</div>
+  )
+}
+
+export default Contacts;
